acfa9f57f8c6a94623524411b820fca1ef597b82
AbdullahBhatti1987 <abdullah.bhatti345@gmail.com>
2026-09-03 13:45:10 +0500

    monthly report update backend link

---
+++ src/api/monthlyIndentApi.js
@@ -0,0 +1,26 @@
+import api from '@/config/api';
+
+export const createMonthlyIndent = async data => {
+  const response = await api.post('/monthly-indents', data);
+  return response.data;
+};
+
+export const getMonthlyIndents = async () => {
+  const response = await api.get('/monthly-indents');
+  return response.data;
+};
+
+export const getMonthlyIndentById = async id => {
+  const response = await api.get(`/monthly-indents/${id}`);
+  return response.data;
+};
+
+export const updateMonthlyIndent = async (id, data) => {
+  const response = await api.put(`/monthly-indents/${id}`, data);
+  return response.data;
+};
+
+export const deleteMonthlyIndent = async id => {
+  const response = await api.delete(`/monthly-indents/${id}`);
+  return response.data;
+};
