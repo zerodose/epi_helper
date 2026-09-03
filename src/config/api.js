435d4163051aa06f48977ff49d4f3aa53c3f6ccf
AbdullahBhatti1987 <abdullah.bhatti345@gmail.com>
2026-09-04 01:42:58 +0500

    backend live

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_BASE_URL = 'https://epi-helper-backend.onrender.com';
-const API_BASE_URL = 'http://192.168.100.12:5000';
+const API_BASE_URL = 'https://epi-helper-backend.onrender.com';
+// const API_BASE_URL = 'http://192.168.100.12:5000';
 
 const api = axios.create({
   baseURL: `${API_BASE_URL}/api`,
